88e225ee7e2757887b65c6d5a9889019244dd2ad
pklacher <pia.lacher04@htl-salzburg.ac.at>
2021-01-10 20:55:41 +0100

    add Timer, clean PlayerController Script and Scene

--- Assets/MyGame/Scripts/Timer.cs
+++ Assets/MyGame/Scripts/Timer.cs
@@ -1,22 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public float timeInSec;
-    // Start is called before the first frame update
+    public TextMeshProUGUI timer;
+    private float timeInSec;
+   
+
     void Start()
     {
         timeInSec = 60f;
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
         timeInSec -= Time.deltaTime;
 
         float minutes = Mathf.FloorToInt(timeInSec / 60);
         float seconds = Mathf.FloorToInt(timeInSec % 60);
+
+        if (timeInSec > 0)
+        {
+            timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            Debug.Log("Stop Game");
+        }
     }
 }
