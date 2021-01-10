0c2ffa859df286ff225740a9dcdf45cd8fb6885f
pklacher <pia.lacher04@htl-salzburg.ac.at>
2021-01-10 17:52:40 +0100

    Player movement horizontal

---
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject player;
+
+    private float moveHorizontal;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        moveHorizontal = moveHorizontal * Time.deltaTime;
+
+        player.GetComponent<Transform>().position = player.GetComponent<Transform>().position + new Vector3(moveHorizontal, 0, 0);
+    }
+}
---
+++ Assets/MyGame/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timeInSec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeInSec = 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeInSec -= Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(timeInSec / 60);
+        float seconds = Mathf.FloorToInt(timeInSec % 60);
+    }
+}
